552d4105e7406f22a7a24e54e182cad9c7837588
Levi Wheatcroft <levi@wheatcroft.com.au>
2020-06-23 19:51:28 +0800

    initial styles

---
+++ less/index.js
@@ -0,0 +1,16 @@
+import {
+  unsafeCSS
+} from 'lit-element'
+import _button from './button.less'
+import _icons from './icons.less'
+import _milligram from './milligram.less'
+import _tag from './tag.less'
+import _grid from './grid.less'
+import _base from './base.less'
+
+export const button = unsafeCSS(_button)
+export const icons = unsafeCSS(_icons)
+export const milligram = unsafeCSS(_milligram)
+export const tag = unsafeCSS(_tag)
+export const grid = unsafeCSS(_grid)
+export const base = unsafeCSS(_base)
