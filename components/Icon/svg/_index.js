5352345b98881914c9e6c6e45edfbf49e685f1fc
Levi Wheatcroft <levi@wheatcrofts.com.au>
2020-07-02 04:45:07 +0800

    Icon svg registry

---
+++ components/Icon/svg/_index.js
@@ -0,0 +1,29 @@
+import atSign from './at-sign.svg'
+import square from './square.svg'
+import x from './x.svg'
+import sliders from './sliders.svg'
+import trash2 from './trash-2.svg'
+import filePlus from './file-plus.svg'
+import cloudLightning from './cloud-lightning.svg'
+import download from './download.svg'
+import search from './search.svg'
+import calendar from './calendar.svg'
+import externalLink from './external-link.svg'
+import checkSquare from './check-square.svg'
+import save from './save.svg'
+
+export default {
+  atSign,
+  square,
+  x,
+  sliders,
+  trash2,
+  filePlus,
+  cloudLightning,
+  download,
+  search,
+  calendar,
+  externalLink,
+  checkSquare,
+  save
+}
