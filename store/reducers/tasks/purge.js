fa5b9c894a433a0b5a681e77ae8ae10d20514453
Levi Wheatcroft <levi@wheatcroft.com.au>
2020-07-09 19:05:01 +0800

    purge tasks only from selectedListId, fixes #7

--- store/reducers/tasks/purge.js
+++ store/reducers/tasks/purge.js
@@ -2,15 +2,18 @@ export function tasksPurge (action, { getState, update }) {
   if (action.type !== 'tasksPurge')
     return
 
-  Object.values(getState().lists).forEach(({ id: listId, tasks }) => {
-    tasks = Object.fromEntries(
-      Object.values(tasks)
-        .map((task) => {
-          if (task.complete)
-            task = { ...task, purged: true }
-          return [task.id, task]
-        })
-    )
-    update(['lists', listId, 'tasks'], tasks)
-  })
+  const {
+    selectedListId,
+    lists
+  } = getState()
+
+  const tasks = Object.fromEntries(
+    Object.values(lists[selectedListId].tasks)
+      .map((task) => {
+        if (task.complete)
+          task = { ...task, purged: true }
+        return [task.id, task]
+      })
+  )
+  update(['lists', selectedListId, 'tasks'], tasks)
 }
