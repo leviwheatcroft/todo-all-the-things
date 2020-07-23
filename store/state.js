ea73bc9134044771a8084199e2065a8fbcf9e0d1
Levi Wheatcroft <levi@wheatcroft.com.au>
2020-07-24 03:57:57 +0800

    add hydrate dehydrate

--- store/state.js
+++ store/state.js
@@ -13,7 +13,8 @@ export const initialState = {
     driver: false,
     state: 'noDriver',
     refreshInterval: 10 * 60 * 1000,
-    accessToken: false
+    accessToken: false,
+    error: false
   },
   settings: {
     includeTasksInErrorReport: false,
@@ -32,4 +33,46 @@ export const initialState = {
   },
   projects: [],
   contexts: []
+}
+
+const dehydrators = {
+  dialogs: () => undefined,
+  sort: ({ sort }) => sort,
+  remoteStorage: ({ remoteStorage }) => {
+    const persisted = ['driver', 'refreshInterval', 'accessToken']
+    return Object.fromEntries(
+      Object.entries(remoteStorage)
+        .filter(([key]) => persisted.includes(key))
+    )
+  },
+  settings: ({ settings }) => settings,
+  filter: ({ filter: { text, regExp } }) => {
+    return { text, regExp: new RegExp(regExp) }
+  },
+  selectedListId: ({ selectedListId }) => selectedListId,
+  version: ({ version }) => version,
+  lists: () => undefined,
+  projects: () => undefined,
+  contexts: () => undefined
+}
+const hydrators = {
+  filter: ({ filter: { text, regExp } }) => {
+    return { text, regExp: new RegExp(regExp) }
+  }
+}
+
+export function dehydrate (state) {
+  return Object.fromEntries(
+    Object.entries(state)
+      .map(([key]) => [key, dehydrators[key](state)])
+      .filter(([, value]) => value !== undefined)
+  )
+}
+export function hydrate (state) {
+  return Object.fromEntries(
+    Object.entries(state)
+      .map(([key, value]) => {
+        return [key, hydrators[key] ? hydrators[key](state) : value]
+      })
+  )
 }
