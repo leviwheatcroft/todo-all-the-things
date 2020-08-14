d9e863aa06ea56b0815241725f56e0dded1a80fe
Levi Wheatcroft <levi@wheatcroft.com.au>
2020-08-15 04:13:16 +0800

    eslint directive

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,7 @@ import { initialiseRemoteStorage } from './remoteStorage'
 import { initialiseOptions } from './options'
 
 export function initialiseControllers () {
+  // eslint-disable-next-line no-new
   new LocalStorage()
   initialiseRemoteStorage()
   initialiseOptions() // this is a more appropriate pattern a than using classes
